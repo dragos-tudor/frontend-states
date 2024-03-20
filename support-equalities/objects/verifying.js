82a6fe842f2e37a2e9452b19ae9c174671871a91
Dragos Tudor <dragos.tudor@gmail.com>
2024-03-20 13:18:02 +0000

    refactor(states): uppercase objects labels.

--- support-equalities/objects/verifying.js
+++ support-equalities/objects/verifying.js
@@ -1,8 +1,8 @@
 
-const reservedPropNames = ["children"]
+const ReservedPropNames = Object.freeze(["children"])
 
 export const existsObject = (obj) => obj != null
 
 export const isObjectType = (value) => typeof value === "object" && value !== null
 
-export const isValidObjectPropName = (propName) => !reservedPropNames.includes(propName)
+export const isValidObjectPropName = (propName) => !ReservedPropNames.includes(propName)
