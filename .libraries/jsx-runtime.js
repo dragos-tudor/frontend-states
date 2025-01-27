a8ca524d17132dd272bf839af241b2f9963b3e33
Dragos Tudor <dragos.tudor@gmail.com>
2025-01-27 19:15:11 +0200

    update(deps): update rendering and jsx runtime libraries.

--- .libraries/jsx-runtime.js
+++ .libraries/jsx-runtime.js
@@ -13,23 +13,26 @@ const createJsxElement = (type, props, key, parent, ref)=>({
         ref,
         _owner: parent
     });
-const getJsxParent = (internals)=>internals?.ReactCurrentOwner?.current;
 const getJsxInternals = (store)=>store?.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
-const ResevedPropNames = Object.freeze({
-    key: undefined,
-    ref: undefined,
-    __self: undefined,
-    __source: undefined
-});
-const existsJsxKey = (key)=>key !== undefined;
+const getJsxParent = (internals)=>internals?.ReactCurrentOwner?.current;
 const getJsxPropsKey = (props)=>props.key;
-const existsJsxPropsKey = (props)=>getJsxPropsKey(props) !== undefined;
 const getJsxPropNames = (props)=>Object.getOwnPropertyNames(props);
 const getJsxPropsRef = (props)=>props.ref;
+const setJsxPropChildren = (props, children)=>setJsxPropValue(props, "children", children);
+const setJsxPropValue = (props, propName, propValue)=>Object.assign(props, {
+        [propName]: propValue
+    });
+const ResevedPropNames = [
+    "key",
+    "ref",
+    "__self",
+    "__source"
+];
+const existsJsxKey = (key)=>key !== undefined;
+const existsJsxPropsKey = (props)=>getJsxPropsKey(props) !== undefined;
 const existsJsxPropsRef = (props)=>getJsxPropsRef(props) !== undefined;
 const existsJsxPropValue = (props, propName)=>props[propName] !== undefined;
-const isReservedJsxPropName = (propName)=>propName in ResevedPropNames;
-const setJsxPropValue = (props, propName, propValue)=>props[propName] = propValue;
+const isReservedJsxPropName = (propName)=>ResevedPropNames.includes(propName);
 const copyJsxProp = (sourceProps)=>(targetProps, propName)=>{
         setJsxPropValue(targetProps, propName, sourceProps[propName]);
         return targetProps;
@@ -39,13 +42,10 @@ const copyValidJsxProps = (sourceProps, targetProps = {})=>getJsxPropNames(sourc
 const resolveJsxPropsKey = (props, maybeKey)=>existsJsxPropsKey(props) && getJsxPropsKey(props).toString() || existsJsxKey(maybeKey) && maybeKey.toString() || null;
 const resolveJsxPropsRef = (props)=>existsJsxPropsRef(props) && getJsxPropsRef(props) || null;
 const resolveJsxProps = (initialProps, type)=>type && type.defaultProps ? copyDefaultJsxProps(type.defaultProps, copyValidJsxProps(initialProps)) : copyValidJsxProps(initialProps);
-const getJsxLegacyChildren = (children)=>children?.length == 1 ? children[0] : children;
-const emptyLegacyJsxChildren = (children)=>!children || children.length === 0;
 const compileJsxExpression = (type, props, maybeKey)=>createJsxElement(type, resolveJsxProps(props, type), resolveJsxPropsKey(props, maybeKey), getJsxParent(getJsxInternals(globalThis["React"])), resolveJsxPropsRef(props));
-const compileLegacyJsxExpression = (type, props, ...children)=>emptyLegacyJsxChildren(children) ? compileJsxExpression(type, props ?? {}) : compileJsxExpression(type, {
-        ...props ?? {},
-        children: getJsxLegacyChildren(children)
-    });
+const toJsxChildreArray = (children)=>children?.length == 1 ? children[0] : children;
+const existsJsxChildren = (children)=>children && children.length > 0;
+const compileLegacyJsxExpression = (type, props, ...children)=>existsJsxChildren(children) ? compileJsxExpression(type, setJsxPropChildren(props ?? {}, toJsxChildreArray(children))) : compileJsxExpression(type, props ?? {});
 export { FragmentType as Fragment };
 export { compileJsxExpression as jsx };
 export { compileJsxExpression as jsxs };
